79eeef63da792ecbca42402745c762f035da2a9e
akutlusenturk <94523984+akutlusenturk@users.noreply.github.com>
2024-09-13 16:56:27 +0300

    Update eei_lib.py

--- eei_lib.py
+++ eei_lib.py
@@ -9,7 +9,7 @@ def azami (dosya, groupby = "Q", minQ = 0.01, minH = 0.01, minPhyd = 0.01):
     df = pd.read_excel("constant_rev/"+dosya,index_col=0)
     df = df.rename(columns={"Flow_Rate":"Q","Valve_Aperture":"D","Head":"H","Hydraulic_Power":"Phyd", \
                        "Suction_Pressure":"P1","Discharge_Pressure":"P2","Active_Power":"Pcons"})
-    df = df.drop(columns=["Date","Time"]
+    df = df.drop(columns=["Date","Time"])
     print("check")
     print(groupby)
     df = df.groupby(groupby,as_index=False).max()
